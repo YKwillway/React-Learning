b44038d9be6d032bf8bccd0b11f09b744c378efa
YKwillway <yuchan0708bicky@gmail.com>
2024-10-15 06:40:01 +0900

    コンポーネントの使い方について

--- src/main.tsx
+++ src/main.tsx
@@ -1,27 +1,15 @@
 import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { App } from './App';
+// ↑import文では拡張子は省略できる。
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
-// Reactで画面を表す関数をコンポーネントという。
-// コンポーネントは大文字始まり。
-const App = () => {
-  // ・複数要素を表現したい場合は、丸括弧()で囲む
-  // ・複数要素を任意の要素で囲む
-  //   ・任意の要素で囲みたくない場合は、<>を使用する。
-  //.    以前は<>に相当する<React.Fragment>があった。
-  return (
-    <>
-      <h1>こんにちは！</h1>
-      <p>お元気ですか？</p>
-    </>
-  )
-}
-
 root.render(
   // 公式が推奨しているStrictModeを使用する
   <StrictMode>
-    <App />
+    {/* 外部に作成したコンポーネントを使用する。import文は自動的に追加される */}
+    <App></App>
   </StrictMode>,
 )
