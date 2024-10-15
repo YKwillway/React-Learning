e688ad934c71d36f8bce13950eb8408987bf676a
YKwillway <yuchan0708bicky@gmail.com>
2024-10-15 19:58:31 +0900

    Styleを指定する

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,20 @@
 // 関数コンポーネント化して、main.tsxから使用する
 // コンポーネント名は大文字始まりのパスカルケースとする
 export const App = () => {
+  // タグの中で関数が長くなりすぎると良くないので、外で関数を定義する
   const onClickButton = () => alert();
+  const h1Style = {
+    color: "green",
+    // 「hoge-fuga」のようなスタイル指定は「hogeFuga」のようなキャメルケースで表記する
+    fontSize: "30px",
+    margin: 100
+  }
   return (
     <>
-      <h1>こんにちは</h1>
-      <p>お元気ですか？</p>
-      {console.log('hoge')}
-      <button onClick={() => alert('hoge')}>ボタン</button>
+      {/* スタイルの「:」の右側はダブルクォーテーションで囲む */}
+      <h1 style={{color: "red"}}>こんにちは</h1>
+      <p style={h1Style}>お元気ですか？</p>
+      <button onClick={onClickButton}>ボタン</button>
     </>
   );
 };
