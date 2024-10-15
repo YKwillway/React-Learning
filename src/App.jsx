71573dc66ae7183621563d6123dcd63c73adef0e
YKwillway <yuchan0708bicky@gmail.com>
2024-10-15 19:50:08 +0900

    HTMLタグの中でJavascriptを実行する

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,13 @@
 // 関数コンポーネント化して、main.tsxから使用する
 // コンポーネント名は大文字始まりのパスカルケースとする
 export const App = () => {
+  const onClickButton = () => alert();
   return (
     <>
       <h1>こんにちは</h1>
       <p>お元気ですか？</p>
+      {console.log('hoge')}
+      <button onClick={() => alert('hoge')}>ボタン</button>
     </>
   );
 };
