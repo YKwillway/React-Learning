94631548399930815d231856e229987fe93acaf0
YKwillway <yuchan0708bicky@gmail.com>
2024-10-16 06:29:34 +0900

    Stateを利用する

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
-// 関数コンポーネントをまとめたファイルは拡張子「.jsx」とするのがgood
-// 関数コンポーネント化して、main.tsxから使用する
-
+import { useState } from "react";
 import { ColorfulMessage } from "./components/ColorfulMessage";
 
 // コンポーネント名は大文字始まりのパスカルケースとする
 export const App = () => {
-  // タグの中で関数が長くなりすぎると良くないので、外で関数を定義する
-  const onClickButton = () => alert();
+  // Reactの機能を接続するための関数を「フック」と呼ぶ
+  // useStateはStateを利用するための「フック」
+  // スコープの最上位階層でないとuseState(初期値)は使用できない
+  const [num, setNum] = useState(0);
+  const onClickCountUp = () => {
+    // Reactのstate更新は特定の関数の中だとバッチ化されて実行される
+    // setNum(num + 1)を2行記述していても、ボタンを押下した際、1カウントアップされる
+    // setNum(num + 1);
+    // setNum(num + 1);
+
+    // set〇〇は引数に関数を受けることもできる。この場合はボタンを押下した際、2カウントアップされる
+    // 変数prevにはsetNum実行時点のnumの値が入ってくる
+    setNum((prev) => prev + 1);
+    setNum((prev) => prev + 1);
+  };
   const contentStyle = {
     color: "green",
     // 「hoge-fuga」のようなスタイル指定は「hogeFuga」のようなキャメルケースで表記する
     fontSize: "18px",
-  }
+  };
   return (
     <>
       {/* スタイルの「:」の右側はダブルクォーテーションで囲む */}
@@ -22,7 +33,8 @@ export const App = () => {
       <ColorfulMessage color="blue" message="元気です"></ColorfulMessage> */}
       <ColorfulMessage color="green">元気ですか！(猪木風)</ColorfulMessage>
       <ColorfulMessage color="orange">元気ですか！(2回目)</ColorfulMessage>
-      <button onClick={onClickButton}>ボタン</button>
+      <button onClick={onClickCountUp}>ボタン</button>
+      <p>{num}</p>
     </>
   );
 };
